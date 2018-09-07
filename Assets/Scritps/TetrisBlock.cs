2fcb7256e9be6eef397d6ed310c19c184ed4f340
Alfredo Barcellos <alfmb@terra.com.br>
2018-09-07 15:51:48 -0300

    Initial commit

---
+++ Assets/Scritps/TetrisBlock.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+[Serializable]
+public class TetrisBlock
+{
+    public int PositionX;
+    public int PositionY;
+}
---
+++ Assets/Scritps/TetrisBlockGroupRotator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class TetrisBlockGroupRotator : MonoBehaviour
+{
+    public TetrisBlockGroup BlockGroup;
+
+    public bool CounterClockWise;
+
+    private void OnEnable()
+    {
+        Rotate(BlockGroup, CounterClockWise);
+    }
+
+    private void Rotate(TetrisBlockGroup blockGroup, bool counterClockWise = false)
+    {
+        int centerX = blockGroup.Pivot.PositionX;
+        int centerY = blockGroup.Pivot.PositionY;
+        var blocks = blockGroup.Blocks;
+
+        int newX, newY;
+        TetrisBlock block;
+        for (int i = 0; i < blocks.Length; i++)
+        {
+            block = blocks[i];
+            newX = block.PositionY - centerY;
+            newY = block.PositionX - centerX;
+            if (counterClockWise)
+            {
+                block.PositionX = centerX - newX;
+                block.PositionY = centerY + newY;
+            }
+            else
+            {
+                block.PositionX = centerX + newX;
+                block.PositionY = centerY - newY;
+            }
+        }
+    }
+}
